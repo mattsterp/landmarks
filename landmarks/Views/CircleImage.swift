a18b9e81bc6a7233fe95eedb79de126cfdf7f130
Matthhew Pters <mattsterp@gmail.com>
2021-04-22 11:35:59 -0400

    Initial Commit

---
+++ landmarks/Views/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Matthew Peters on 4/21/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Matthew Peters on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
