df911a8732227033de214f306b615ec2ba341f89
Matthhew Pters <mattsterp@gmail.com>
2021-04-22 15:09:12 -0400

    setup navigation between list and detail view

---
+++ landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Matthew Peters on 4/22/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+
+
